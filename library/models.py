1be4e6b881959f3cf3a78eccb90961e7942fb6a6
gustavo <contato@gustavohalm.com>
2020-07-06 23:39:06 -0300

    books endpoint

--- library/models.py
+++ library/models.py
@@ -6,3 +6,13 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    edition = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    authors = models.ManyToManyField(Author, related_name='books')
+
+    def __str__(self):
+        return self.name
